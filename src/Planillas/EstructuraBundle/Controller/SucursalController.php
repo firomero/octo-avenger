2c9c7321e89be103fcf31314b505cab5132d7542
Dundivet <dundivet@emailn.de>
2014-04-06 17:43:52 -0400

    actualizadas dependencias para sonata admin

---
+++ src/Planillas/EstructuraBundle/Controller/SucursalController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cinfante
+ * Date: 06/04/14
+ * Time: 05:22 PM
+ */
+
+namespace Planillas\EstructuraBundle\Controller;
+
+
+class SucursalController {
+
+} 
---
+++ src/Planillas/EstructuraBundle/Controller/TurnoController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cinfante
+ * Date: 06/04/14
+ * Time: 05:23 PM
+ */
+
+namespace Planillas\EstructuraBundle\Controller;
+
+
+class TurnoController {
+
+} 
