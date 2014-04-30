8a7a11df3b6f0717893472272c29a993cb19aefc
Dundivet <dundivet@emailn.de>
2014-04-30 11:44:36 -0400

    hotfix en controlador de salario base para asimilar el cambio en componentes salariales

--- src/Planillas/EntidadesBundle/Controller/EComponentesSalarialesController.php
+++ src/Planillas/EntidadesBundle/Controller/EComponentesSalarialesController.php
@@ -32,7 +32,12 @@ class EComponentesSalarialesController extends Controller {
     public function componentesByIdEmpleadoAction($id_empleado) {
         $em = $this->getDoctrine()->getManager();
 
-        $entities = $em->getRepository('PlanillasEntidadesBundle:EComponentesSalariales')->findBy(array('empleado' => $id_empleado,  'planilla' => null, 'deleted_at' => null));
+        $entities = $em->getRepository('PlanillasEntidadesBundle:EComponentesSalariales')->findBy(
+            array(
+                'empleado' => $id_empleado,
+                'planillaEmpleado' => null,
+                'deleted_at' => null
+            ));
         $aDeleteForm = array();
         foreach ($entities as $entity) {
 
