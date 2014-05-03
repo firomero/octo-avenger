88173a95e4ee68c7fd2080e1bb191fa2fa910484
Dundivet <dundivet@emailn.de>
2014-05-03 02:28:14 -0400

    typo en use BuscarIncapacidadesType

---
+++ src/Planillas/PaymentsBundle/Managers/PDFExport/AbstractPDFExporter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cinfante
+ * Date: 02/05/14
+ * Time: 03:54 PM
+ */
+
+namespace Planillas\PaymentsBundle\Managers\PDFExport;
+
+
+class AbstractPDFExporter {
+
+} 
